b308514e5eac3cf5a7374ad2a712008038fd0913
iTechEverything <omchachad12@gmail.com>
2024-07-09 11:11:11 +0530

    New Project | Initial Commit

---
+++ Krupa's Foods/Krupa_s_FoodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Krupa_s_FoodsApp.swift
+//  Krupa's Foods
+//
+//  Created by Om Chachad on 09/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Krupa_s_FoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
