049b749a6581c7b19341a82ae46b222830581a9b
iTechEverything <omchachad12@gmail.com>
2024-07-30 11:23:36 +0530

    Toolbar Background Set to Hidden to allow for Variable Blur

--- Krupa's Foods/Krupas_FoodsApp.swift
+++ Krupa's Foods/Krupas_FoodsApp.swift
@@ -16,4 +16,20 @@ struct Krupas_FoodsApp: App {
         .modelContainer(for: [Order.self, Product.self, Customer.self])
     }
     
+    init() {
+        UINavigationBar.appearance().barTintColor = .clear
+        UINavigationBar.appearance().backgroundColor = .clear
+        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
+        UINavigationBar.appearance().shadowImage = UIImage()
+        
+        let appearance = UITabBarAppearance()
+            appearance.configureWithTransparentBackground() // <- HERE
+//            appearance.stackedLayoutAppearance.normal.iconColor = .white
+//            appearance.stackedLayoutAppearance.normal.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+
+//            appearance.stackedLayoutAppearance.selected.iconColor = UIColor(Color.accentColor)
+//            appearance.stackedLayoutAppearance.selected.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor(Color.accentColor)]
+
+            UITabBar.appearance().standardAppearance = appearance
+    }
 }
