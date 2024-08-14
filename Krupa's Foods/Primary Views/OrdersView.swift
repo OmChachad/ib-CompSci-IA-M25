d8bd05eef91d387313c624035af6890e1f8d0ca3
Om Chachad <omchachad12@gmail.com>
2024-08-14 13:34:43 +0530

    Header is now permanently attached to the Top Safe Area Inset

--- Krupa's Foods/Primary Views/OrdersView.swift
+++ Krupa's Foods/Primary Views/OrdersView.swift
@@ -34,93 +34,92 @@ struct OrdersView: View {
     }
     
     var body: some View {
-        ScrollView {
-            LazyVStack(pinnedViews: .sectionHeaders) {
-                Section {
-                    if orders.isEmpty {
-                        ContentUnavailableView("No Orders Placed", systemImage: "shippingbox.fill", description: Text("Click \(Image(systemName: "plus.circle.fill")) to add your first order."))
-                            .frame(maxHeight: .infinity, alignment: .center)
-                    } else {
-                        VStack {
+        VStack {
+            if orders.isEmpty {
+                ContentUnavailableView("No Orders Placed", systemImage: "shippingbox.fill", description: Text("Click \(Image(systemName: "plus.circle.fill")) to add your first order."))
+                    .frame(maxHeight: .infinity, alignment: .center)
+            } else {
+                ScrollView {
+                    LazyVStack {
+                        HStack {
+                            VStack(spacing: 0) {
+                                Text("\(pendingOrdersCount)")
+                                    .font(.title.bold())
+                                Text("Pending")
+                            }
+                            .opacity(0.8)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(.orange.gradient.opacity(0.2), in: .rect(cornerRadius: 20, style: .continuous))
+                            .padding(2.5)
+                            
+                            VStack(spacing: 0) {
+                                Text("\(completedOrdersCount)")
+                                    .font(.title.bold())
+                                Text("Completed")
+                            }
+                            .opacity(0.8)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(.green.gradient.opacity(0.3), in: .rect(cornerRadius: 20, style: .continuous))
+                            .padding(2.5)
+                        }
+                        .padding(.horizontal, 12.5)
+                        
+                        
+                        ForEach(orders) { order in
                             HStack {
-                                VStack(spacing: 0) {
-                                    Text("\(pendingOrdersCount)")
-                                        .font(.title.bold())
-                                    Text("Pending")
-                                }
-                                .opacity(0.8)
-                                .padding()
-                                .frame(maxWidth: .infinity)
-                                .background(.orange.gradient.opacity(0.2), in: .rect(cornerRadius: 20, style: .continuous))
-                                .padding(2.5)
+                                Text(order.product.icon)
+                                    .font(.largeTitle)
                                 
-                                VStack(spacing: 0) {
-                                    Text("\(completedOrdersCount)")
-                                        .font(.title.bold())
-                                    Text("Completed")
+                                VStack(alignment: .leading) {
+                                    Text(order.customer.name)
+                                        .bold()
+                                    Text(order.customer.address.line1)
+                                        .foregroundStyle(.secondary)
                                 }
-                                .opacity(0.8)
-                                .padding()
-                                .frame(maxWidth: .infinity)
-                                .background(.green.gradient.opacity(0.3), in: .rect(cornerRadius: 20, style: .continuous))
-                                .padding(2.5)
-                            }
-                            .padding(.horizontal, 12.5)
-                            
-                            
-                            ForEach(orders) { order in
-                                HStack {
-                                    Text(order.product.icon)
-                                        .font(.largeTitle)
-                                    
-                                    VStack(alignment: .leading) {
-                                        Text(order.customer.name)
-                                            .bold()
-                                        Text(order.customer.address.line1)
-                                            .foregroundStyle(.secondary)
-                                    }
-                                    
-                                    Spacer()
-                                    
-                                    VStack(alignment: .leading) {
-                                        Text("^[\(order.quantity.formatted()) \(order.product.measurementUnit.rawValue.capitalized)](inflect: true)")
-                                        Text(order.amountPaid, format: .currency(code: "INR"))
-                                            .foregroundStyle(.secondary)
-                                    }
+                                
+                                Spacer()
+                                
+                                VStack(alignment: .leading) {
+                                    Text("^[\(order.quantity.formatted()) \(order.product.measurementUnit.rawValue.capitalized)](inflect: true)")
+                                    Text(order.amountPaid, format: .currency(code: "INR"))
+                                        .foregroundStyle(.secondary)
                                 }
-                                .padding(10)
-                                .background(.ultraThickMaterial, in: .rect(cornerRadius: 20, style: .continuous))
-                                .padding(.horizontal)
-                                .padding(.vertical, 2.5)
                             }
+                            .padding(10)
+                            .background(.ultraThickMaterial, in: .rect(cornerRadius: 20, style: .continuous))
+                            .padding(.horizontal)
+                            .padding(.vertical, 2.5)
                         }
-                    }
-                } header: {
-                    HStack {
-                        Text("Orders")
-                            .font(.largeTitle.bold())
-                        
-                        Spacer()
-                        
-                        Button("Add Order", systemImage: "plus.circle.fill") {
-                            showingNewOrderView = true
-                        }
-                        .labelStyle(.iconOnly)
-                        .imageScale(.large)
-                    }
-                    .padding(.horizontal)
-                    .background {
-                        Rectangle()
-                            .fill(.clear)
-                            .background(.bar)
-                            .blur(radius: 10)
-                            .padding([.top, .horizontal], -100)
                     }
                 }
             }
-            
-            .padding(.bottom, 125)
         }
+        .padding(.bottom, 125)
+    
+        .safeAreaInset(edge: .top, content: {
+            HStack {
+                Text("Orders")
+                    .font(.largeTitle.bold())
+                
+                Spacer()
+                
+                Button("Add Order", systemImage: "plus.circle.fill") {
+                    showingNewOrderView = true
+                }
+                .labelStyle(.iconOnly)
+                .imageScale(.large)
+            }
+            .padding(.horizontal)
+            .background {
+                Rectangle()
+                    .fill(.clear)
+                    .background(.bar)
+                    .blur(radius: 10)
+                    .padding([.top, .horizontal], -100)
+            }
+        })
         .sheet(isPresented: $showingNewOrderView) {
             AddOrderView(product: product)
         }
