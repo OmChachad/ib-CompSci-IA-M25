07ac1f23a70ee10d1e69d96c7eb5651d86e9b465
Om Chachad <omchachad12@gmail.com>
2024-11-12 09:25:38 +0530

    Added the ability to expand Stock Orders for more details

--- Krupa's Foods/Primary Views/StockItemView.swift
+++ Krupa's Foods/Primary Views/StockItemView.swift
@@ -18,31 +18,85 @@ struct StockItemView: View {
     
     @State private var showDeleteConfirmation = false
     
+    @State private var showingDetails = false
+    
+    var associatedOrders: [Order] {
+        if let fulfilledStock = stockOrder.fulfillingStock {
+            return stockOrder.wrappedUsedBy + fulfilledStock.filter { !stockOrder.wrappedUsedBy.contains($0.order!) }.compactMap { $0.order }
+        } else {
+            return stockOrder.wrappedUsedBy
+        }
+    }
+    
     var body: some View {
         SwipeView {
-            HStack {
-                Image(systemName: "shippingbox.fill")
-                    .foregroundStyle(.yellow.gradient)
-                    .font(.largeTitle)
-                
-                VStack(alignment: .leading) {
-                    Text("^[\(stockOrder.quantityLeft.formatted())/\(stockOrder.quantityPurchased.formatted()) \(stockOrder.wrappedProduct.measurementUnit.title)](inflect: true) remaining")
-                        .bold()
-                    Text(stockOrder.date.formatted(date: .abbreviated, time: .omitted))
+            VStack {
+                HStack {
+                    Image(systemName: "shippingbox.fill")
+                        .foregroundStyle(.yellow.gradient)
+                        .font(.largeTitle)
+                    
+                    VStack(alignment: .leading) {
+                        Text("^[\(stockOrder.quantityLeft.formatted())/\(stockOrder.quantityPurchased.formatted()) \(stockOrder.wrappedProduct.measurementUnit.title)](inflect: true) remaining")
+                            .bold()
+                        Text(stockOrder.date.formatted(date: .abbreviated, time: .omitted))
+                            .foregroundStyle(.secondary)
+                    }
+                    
+                    Spacer()
+                    
+                    Image(systemName: "chevron.right")
+                        .labelStyle(.iconOnly)
                         .foregroundStyle(.secondary)
+                        .rotationEffect(.degrees(showingDetails ? 90 : 0))
+                        .accessibilityHint("\(showingDetails ? "Hide" : "Show") Details for Stock Order on \(stockOrder.date.formatted())")
                 }
+                .strikethrough(stockOrder.quantityLeft == 0 && !showingDetails)
+                .opacity((stockOrder.quantityLeft == 0 && showingDetails) ? 0.6 : 1)
                 
-                Spacer()
-                
-                Text(INRFormatter.string(from: NSNumber(value: stockOrder.amountPaid)) ?? "")
-                    .foregroundStyle(.secondary)
+                if showingDetails {
+                    VStack(alignment: .leading) {
+                        Divider()
+                        
+                        LabeledContent("Amount Paid", value: "\(INRFormatter.string(from: NSNumber(value: stockOrder.amountPaid)) ?? "")")
+                        
+                        if !stockOrder.wrappedUsedBy.isEmpty {
+                            Divider()
+                            
+                            Text("Associated Orders:")
+                                .bold()
+                                .font(.title3)
+                            
+                            ForEach(stockOrder.wrappedUsedBy) { order in
+                                HStack {
+                                    VStack(alignment: .leading) {
+                                        Text(order.wrappedCustomer.name)
+                                            .bold()
+                                        Text(order.amountPaid, format: .currency(code: "INR"))
+                                            .foregroundStyle(.secondary)
+                                    }
+                                    
+                                    Spacer()
+                                    
+                                    Text("\(order.quantity.formatted())")
+                                }
+                            }
+                            .padding(5)
+                        }
+                    }
+                    .opacity(showingDetails ? 1 : 0)
+                    .transition(.blurReplace)
+                }
             }
             .padding(10)
             .background(.ultraThickMaterial, in: .rect(cornerRadius: 20, style: .continuous))
-            .strikethrough(stockOrder.quantityLeft == 0)
-            .opacity(stockOrder.quantityLeft == 0 ? 0.6 : 1)
+            .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading).combined(with: .swipeDelete)))
+            .onTapGesture {
+                showingDetails.toggle()
+            }
         } trailingActions: { context in
             SwipeAction("Delete", systemImage: "trash", backgroundColor: .red) {
+                showingDetails = false
                 showDeleteConfirmation = true
             }
             .allowSwipeToTrigger()
@@ -52,6 +106,7 @@ struct StockItemView: View {
                 }
                 
                 Button("Cancel", role: .cancel) {
+                    showingDetails = false
                     context.state.wrappedValue = .closed
                 }
             } message: {
@@ -59,6 +114,6 @@ struct StockItemView: View {
             }
         }
         .swipeActionCornerRadius(20)
-        .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading).combined(with: .swipeDelete)))
+        .animation(.bouncy, value: showingDetails)
     }
 }
