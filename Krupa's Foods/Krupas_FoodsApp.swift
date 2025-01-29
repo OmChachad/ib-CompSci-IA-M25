cb13ab61846fd33d74c5ff2a53a2fe4b928866f8
Om Chachad <omchachad12@gmail.com>
2025-01-29 10:55:40 +0530

    Removed Debugging Statement

--- Krupa's Foods/Krupas_FoodsApp.swift
+++ Krupa's Foods/Krupas_FoodsApp.swift
@@ -12,14 +12,6 @@ struct Krupas_FoodsApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .onAppear {
-                    let urlApp = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).last
-                           let url = urlApp!.appendingPathComponent("default.store")
-                           if FileManager.default.fileExists(atPath: url.path) {
-                               print("swiftdata db at \(url)")
-                           }
-
-                }
         }
         .modelContainer(for: [Order.self, Product.self, Customer.self])
     }
